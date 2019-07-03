3c4273051c87b5761c753c2d99532c8973fda483
dgvnorth <campello03@hotmail.com>
2019-07-03 14:18:48 +0100

    testing david

--- models/model.js
+++ models/model.js
@@ -4,7 +4,7 @@ const fetchScrape = req => {
   console.log("in model");
   https.request(req, res => {
     console.log(res);
-  }); //need to send a get request here to the targetUrl
+  });
 };
 
 module.exports = fetchScrape;
